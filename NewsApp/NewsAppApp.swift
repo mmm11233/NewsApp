241c2b6a245bf3d63e9b06c698e1554f0540a54e
Mariam Joglidze <mariamjoglidze1@gmail.com>
2023-12-27 12:16:48 +0400

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Mariam Joglidze on 27.12.23.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
